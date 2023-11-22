57ef38439e7f54abb0079267643f12187da0b14c
Larissa <10475169697@estudante.ufjf.br>
2023-11-21 23:06:07 -0300

    cria funções para fazer os botões funcionarem

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getLista } from './src/lista.js';
+import { getLista, adicionaNaLista, removeDaLista, limpaLista } from './src/lista.js';
 
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
@@ -6,8 +6,26 @@ const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
 atualizarListaOrdenada();
+
+btnAdicionar.addEventListener('click', adicionaItemDaEntrada);
+
+btnLimpar.addEventListener('click', limparListaOrdenada);
+
+function adicionaItemDaEntrada(){
+    const valor = pEntrada.textContent;
+    adicionaNaLista(valor);
+    pEntrada.textContent = "";
+    atualizarListaOrdenada();
+}
+
+function limparListaOrdenada(){
+    limpaLista();
+    atualizarListaOrdenada();
+}
+
 function atualizarListaOrdenada(){
     const lista = getLista();
+    olItens.innerHTML = "";
     for(let i=0; i<lista.length; i++){
         adicionaElementoNaListaOrdenada(lista[i]);
     }
